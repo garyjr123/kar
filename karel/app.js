c072b66db7aea6eb2f68ccb50db7618c1821d2dc
garyjr123 <garyfmeramontejr@gmail.com>
2025-02-23 14:18:17 +0800

    Update app.js

--- karel/app.js
+++ karel/app.js
@@ -4,3 +4,13 @@ onload = () => {
       clearTimeout(c);
     }, 1000);
   };
+
+  function go(){
+    const b = new Audio();
+    b.src = "song.mp3";
+    b.play();
+    document.getElementById("cli").style='display:none;';
+  }
+  setTimeout(function(){
+go()
+  },1000);
